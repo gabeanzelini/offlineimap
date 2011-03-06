01dd1b0a2efdfe4ce1d404a8fb8ba77b7213daf5
Sebastian Spaeth <Sebastian@SSpaeth.de>
2011-03-06 16:51:37 +0100

    Use self.doautosave rather than self.dofsync
    doautosave was a useless variable before (it was *always* 1). So we
    remove the self.dofsync variable and store in doautosave whether we
    should fsync as often as possible (which really hurts performance).
    
    The sqlite backend could (at one point) use the doautosave variable to
    determine if it should autocommit after each modification.
    
    Signed-off-by: Sebastian Spaeth <Sebastian@SSpaeth.de>

--- offlineimap/folder/LocalStatus.py
+++ offlineimap/folder/LocalStatus.py
@@ -28,12 +28,12 @@ class LocalStatusFolder(BaseFolder):
         self.root = root
         self.sep = '.'
         self.config = config
-        self.dofsync = config.getdefaultboolean("general", "fsync", True)
         self.filename = repository.getfolderfilename(name)
         self.messagelist = None
         self.repository = repository
         self.savelock = threading.Lock()
-        self.doautosave = 1
+        self.doautosave = config.getdefaultboolean("general", "fsync", False)
+        """Should we perform fsyncs as often as possible?"""
         self.accountname = accountname
         super(LocalStatusFolder, self).__init__()
 
@@ -83,10 +83,6 @@ class LocalStatusFolder(BaseFolder):
             self.messagelist[uid] = {'uid': uid, 'flags': flags}
         file.close()
 
-    def autosave(self):
-        if self.doautosave:
-            self.save()
-
     def save(self):
         self.savelock.acquire()
         try:
@@ -98,12 +94,12 @@ class LocalStatusFolder(BaseFolder):
                 flags = ''.join(flags)
                 file.write("%s:%s\n" % (msg['uid'], flags))
             file.flush()
-            if self.dofsync:
+            if self.doautosave:
                 os.fsync(file.fileno())
             file.close()
             os.rename(self.filename + ".tmp", self.filename)
 
-            if self.dofsync:
+            if self.doautosave:
                 fd = os.open(os.path.dirname(self.filename), os.O_RDONLY)
                 os.fsync(fd)
                 os.close(fd)
@@ -125,7 +121,7 @@ class LocalStatusFolder(BaseFolder):
             return uid
 
         self.messagelist[uid] = {'uid': uid, 'flags': flags, 'time': rtime}
-        self.autosave()
+        self.save()
         return uid
 
     def getmessageflags(self, uid):
@@ -136,7 +132,7 @@ class LocalStatusFolder(BaseFolder):
 
     def savemessageflags(self, uid, flags):
         self.messagelist[uid]['flags'] = flags
-        self.autosave()
+        self.save()
 
     def deletemessage(self, uid):
         self.deletemessages([uid])
@@ -149,4 +145,4 @@ class LocalStatusFolder(BaseFolder):
 
         for uid in uidlist:
             del(self.messagelist[uid])
-        self.autosave()
+        self.save()
